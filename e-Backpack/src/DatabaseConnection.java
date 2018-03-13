679f8fa87e41070904f660293279edc623c35d1c
y1cao <y1cao@VPLinCSCI013.ad.csbsju.edu>
2018-03-13 12:10:17 -0500

    db controller

---
+++ e-Backpack/src/DatabaseConnection.java
@@ -0,0 +1,28 @@
+
+import java.io.*;
+import java.sql.*;
+
+/**
+ *
+ * @author APEC
+*/
+
+@SuppressWarnings("serial")
+public class DatabaseConnection implements Serializable {
+	private static Connection  myConnection;
+ 
+	public static Connection openDBConnection() {
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+			myConnection = DriverManager.getConnection("jdbc:oracle:thin:@//cscioraclesrv.ad.csbsju.edu:1521/csci.cscioraclesrv.ad.csbsju.edu","team3", "bccdw");
+			return myConnection;
+		} catch (SQLException | ClassNotFoundException E) { }
+		return null;
+}
+ 
+public static void closeDBConnection() {
+	 try {
+		 myConnection.close();
+	 } catch (SQLException E) { }
+} 
+}
